96e8119150198463b121aa15dc489dea28c2731c
sunilkanna <132731402+sunilkanna@users.noreply.github.com>
2024-07-18 18:52:19 +0530

    Create 2.Copy contents of one file to another file.cpp

---
+++ 2.Copy contents of one file to another file.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h> // For exit()
+
+int main()
+{
+    FILE *fptr1, *fptr2;
+    char filename[100];
+    int c;
+
+    printf("Enter the filename to open for reading: ");
+    scanf("%s", filename);
+
+    // Open one file for reading
+    fptr1 = fopen(filename, "r");
+    if (fptr1 == NULL)
+    {
+        printf("Cannot open file %s\n", filename);
+        exit(1);
+    }
+
+    printf("Enter the filename to open for writing: ");
+    scanf("%s", filename);
+
+    // Open another file for writing
+    fptr2 = fopen(filename, "w");
+    if (fptr2 == NULL)
+    {
+        printf("Cannot open file %s\n", filename);
+        exit(1);
+    }
+
+    // Read contents from file
+    while ((c = fgetc(fptr1)) != EOF)
+    {
+        fputc(c, fptr2);
+    }
+
+    printf("Contents copied to %s\n", filename);
+
+    fclose(fptr1);
+    fclose(fptr2);
+    return 0;
+}
